13c3c2e8e9239418a943c560356eddcba19b150b
Omid Gholamalamdari <omid.alamdari@gmail.com>
2021-04-24 18:15:37 -0500

    The versionr equirement for scikit-image removed .DS_Store

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='angler',
       license='BSD',
       packages=find_packages(),
       install_requires=[
-          'scikit-image==0.15.*',
+          'scikit-image',
           'numpy',
           'scipy',
           'matplotlib',
